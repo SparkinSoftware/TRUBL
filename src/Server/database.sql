053ccce2bcf79eb8d2992638dcc2106e2f71acdd
Colin Tolliver <colintolliver@gmail.com>
2024-01-18 12:17:02 -0800

    Backend boiler

---
+++ src/Server/database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Employee (
+    ID serial PRIMARY KEY,
+    Admin BOOLEAN,
+    Tech BOOLEAN,
+    skillset varchar,
+    location varchar,
+    name varchar,
+    email varchar,
+    password varchar
+);
+CREATE TABLE TaskIssue (
+    ID serial PRIMARY KEY,
+    Emp_ID int REFERENCES Employee(ID),
+    Location varchar,
+    Description varchar,
+    Status varchar,
+    category varchar,
+    remote BOOLEAN
+);
