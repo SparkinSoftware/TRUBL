412f41087fcd1ccb0fc2adad4dadf9f34858cc73
Jesse_Carter <jessetcarter@hotmail.com>
2024-01-19 14:28:19 -0700

    admin containers created

--- src/components/Administrator.jsx
+++ src/components/Administrator.jsx
@@ -5,10 +5,10 @@ const Administrator =({}) => {
     return (
         <>
             <div className='administrator-container'>
-                <div className='administrator-employees'>
+                <div className='administrator-employees-view'>
                     <div className='administrator-title'>{`Administrator Portal`}</div>
                     <div className='administrator-employeesContainer'>
-                        {/* FIXME: Tickets Component goes here (filtered)*/}
+                        {/* FIXME: EmployeesView Component goes here */}
                     </div>
                 </div>
             </div>
