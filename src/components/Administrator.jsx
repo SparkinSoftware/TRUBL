1ac489535b699c20453803b10f809bf074b20679
Jesse_Carter <jessetcarter@hotmail.com>
2024-01-19 15:48:35 -0700

    symbols incorporated

--- src/components/Administrator.jsx
+++ src/components/Administrator.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import addSymbol from '../assets/symbol_add.svg'
+import addFilter from '../assets/symbol_filter.svg'
 
 const Administrator =({}) => {
     return (
@@ -7,8 +9,18 @@ const Administrator =({}) => {
             <div className='administrator-container'>
                 <div className='administrator-employees-view'>
                     <div className='administrator-title'>{`Administrator Portal`}</div>
-                    <div className='administrator-employeesContainer'>
-                        {/* FIXME: EmployeesView Component goes here */}
+                    <div className='administrator-outer-container'>
+                        <div className='administrator-menu-container'>
+                            <div className='administrator-employee-form'>
+                                <img src={addSymbol} class='addSymbol' alt='Add' />                            
+                            </div>
+                            <div className='administrator-employee-filter'>
+                                <img src={addFilter} class='addFilter' alt='Filter' />                            
+                            </div>
+                        </div>
+                        <div className='administrator-inner-container'>
+                            {/* FIXME: EmployeesView Component goes here */}
+                        </div>
                     </div>
                 </div>
             </div>
