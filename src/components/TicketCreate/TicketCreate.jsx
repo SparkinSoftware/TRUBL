dfd6cdc1efd6721dc83e5b9ba6b48b97dea1309a
Patino5 <106916823+Patino5@users.noreply.github.com>
2024-01-23 17:48:05 -0600

    updated naming of input values to match database.

--- src/components/TicketCreate/TicketCreate.jsx
+++ src/components/TicketCreate/TicketCreate.jsx
@@ -1,151 +1,3 @@
-// import React, { useState, useEffect  } from "react";
-// import { Link } from "react-router-dom";
-// import { useSupabase } from '../../SupabaseContext';
-// import './ticket.css';
-
-// const TicketCreation = () => {
-//     const supabase = useSupabase();
-//     const [submittedTicket, setSubmittedTicket] = useState({
-//         ticketCategory: "",
-//         ticketLocation: "",
-//         descriptionOfIssue: ""
-//     })
-    
-//     const [showForm, setShowForm] = useState(false)
-//     const [pendingTickets, setPendingTickets] = useState([])
-
-//     const handleNewIssueClick = () => { setShowForm(true); }
-//     const handleCloseForm = () => { setShowForm(false); }
-
-//     useEffect(() => {
-//         const fetchTickets = async () => {
-//             const { data, error } = await supabase
-//                 .from('taskissue')
-//                 .select('*');
-
-//             if (error) {
-//                 console.error('Error fetching tickets:', error.message);
-//             } else {
-//                 setPendingTickets(data);
-//                 console.log(pendingTickets);
-//             }
-//         };
-
-//         fetchTickets();
-//     }, []);
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         console.log(supabase.auth.getUser())
-    
-//         setPendingTickets([...pendingTickets, submittedTicket]);
-    
-//         const { data, error } = await supabase
-//             .from('taskissue')
-//             .insert([
-//                 {
-//                     location: submittedTicket.ticketLocation,
-//                     description: submittedTicket.descriptionOfIssue,
-//                     status: 'Pending',
-//                     category: submittedTicket.ticketCategory,
-//                 }
-//             ]);
-    
-//         if (error) {
-//             console.error('Error inserting data:', error.message);
-//         } else {
-//             console.log('Data inserted:', data);
-//         }
-    
-//         setSubmittedTicket({
-//             category: "",
-//             location: "",
-//             description: ""
-//         });
-    
-//         setShowForm(false);
-//     };
-
-//     const handleInputChange = (e) => {
-//         const {name, value} = e.target
-//         setSubmittedTicket({
-//             ...submittedTicket,
-//             [name]: value
-//         })
-//     }
-
-
-//     const handleDeleteTicket = (index) => {
-//         // Remove the ticket at the specified index from pending tickets
-//         const updatedTickets = [...pendingTickets];
-//         updatedTickets.splice(index, 1);
-//         setPendingTickets(updatedTickets);
-
-//         // Add logic to delete the ticket from the database if needed
-//         // Example: Call an API endpoint to delete the ticket from the database
-//         // deleteTicketFromDatabase(pendingTickets[index].id);
-//     }
-
-//     return (
-//         <div className="ticketPageContainer">
-//             <div className="ticketStatusContainer">
-//                 <h1>Outstanding Ticket Status</h1>
-//                 {/* Tickets area */}
-//                 {pendingTickets.length > 0 && pendingTickets.map((ticket, index) => (
-            
-//                     <div key={index} className="submittedTicket">
-//                         <p>Category: {ticket.category} Location: {ticket.location} Description: {ticket.description}</p>
-//                         <button id="submittedTicketDelete"onClick={() => handleDeleteTicket(index) }>delete</button>
-//                         <div>
-//                             {/* Progress Bar */}
-//                         </div>
-//                     </div>
-//                 ))}
-                
-//             </div>
-//             {/* Show Form or new Issue button */}
-//             {showForm ? (
-//                 <div className="ticketFormContainer">
-//                     <div id='ticketFormClose' onClick={handleCloseForm}>x</div>
-//                     <form className="ticketForm" onSubmit={handleSubmit}>
-//                         <h3>Please Enter Ticket Information</h3>
-//                         {/* Dropdown for Category */}
-//                         <select name="category" id="ticketCat" onChange={handleInputChange} required>
-//                             <option value="">Issue Category</option>
-//                             <option value="Hardware">Hardware</option>
-//                             <option value="IT">IT</option>
-//                             <option value="Security">Security</option>
-//                             <option value="Software">Software</option>
-//                             <option value="Miscellaneous">Miscellaneous</option>
-//                         </select>
-//                         {/* Dropdown for City */}
-//                         <select name="location" id="ticketLocation" onChange={handleInputChange} required>
-//                             <option value="">Select a City</option>
-//                             <option value="Austin">Austin, TX</option>
-//                             <option value="Dallas">Dallas, TX</option>
-//                             <option value="Houston">Houston, TX</option>
-//                             <option value="SanAntonio">San Antonio, TX</option>
-//                             <option value="LasVegas">Las Vegas, NV</option>
-//                             <option value="Carlsbad">Carlsbad, NM</option>
-//                             {/* Add more options as needed */}
-//                         </select>
-//                         {/* Description of Issue */}
-//                         <textarea name="description" id="ticketDescipt" rows="5" placeholder="Description of Issue..."  onChange={handleInputChange} required />
-//                         <input type="submit" className="createSubmit"/>
-//                     </form>
-//                 </div>
-//             ) : (
-//                 <button onClick={handleNewIssueClick} id="newIssueButton">New Issue</button>
-//             )}
-//             <div id="ticketLinkContainer">
-//                 <Link to='/'>Back Home</Link>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default TicketCreation;
-
 import React, { useState, useEffect  } from "react";
 import { Link } from "react-router-dom";
 import { useSupabase } from '../../SupabaseContext';
@@ -155,9 +7,9 @@ import './ticket.css';
 const TicketCreation = () => {
     const supabase = useSupabase()
     const [submittedTicket, setSubmittedTicket] = useState({
-        ticketCategory: "",
-        ticketLocation: "",
-        descriptionOfIssue: ""
+        category: "",
+        location: "",
+        description: ""
     });
 
     const [showForm, setShowForm] = useState(false);
@@ -193,10 +45,10 @@ const TicketCreation = () => {
             .from('taskissue')
             .insert([
                 {
-                    location: submittedTicket.ticketLocation,
-                    description: submittedTicket.descriptionOfIssue,
+                    location: submittedTicket.location,
+                    description: submittedTicket.description,
                     status: 'Pending',
-                    category: submittedTicket.ticketCategory,
+                    category: submittedTicket.category,
                 }
             ]);
     
@@ -252,9 +104,9 @@ const TicketCreation = () => {
                         <tbody>
                             {pendingTickets.map((ticket, index) => (
                                 <tr key={index}>
-                                    <td>{ticket.ticketCategory}</td>
-                                    <td>{ticket.ticketLocation}</td>
-                                    <td>{ticket.descriptionOfIssue}</td>
+                                    <td>{ticket.category}</td>
+                                    <td>{ticket.location}</td>
+                                    <td>{ticket.description}</td>
                                     <td>
                                         <button onClick={() => handleDeleteTicket(index)}>Delete</button>
                                     </td>
@@ -272,9 +124,9 @@ const TicketCreation = () => {
                         <h3>Please Enter Ticket Information</h3>
                         {/* Dropdown for Category */}
                         <select
-                            name="ticketCategory"
+                            name="category"
                             id="ticketCat"
-                            value={submittedTicket.ticketCategory}
+                            value={submittedTicket.category}
                             onChange={handleInputChange}
                             required
                         >
@@ -287,9 +139,9 @@ const TicketCreation = () => {
                         </select>
                         {/* Dropdown for City */}
                         <select
-                            name="ticketLocation"
+                            name="location"
                             id="ticketLocation"
-                            value={submittedTicket.ticketLocation}
+                            value={submittedTicket.location}
                             onChange={handleInputChange}
                             required
                         >
@@ -304,11 +156,11 @@ const TicketCreation = () => {
                         </select>
                         {/* Description of Issue */}
                         <textarea
-                            name="descriptionOfIssue"
+                            name="description"
                             id="ticketDescipt"
                             rows="5"
                             placeholder="Description of Issue..."
-                            value={submittedTicket.descriptionOfIssue}
+                            value={submittedTicket.description}
                             onChange={handleInputChange}
                             required
                         />
@@ -318,6 +170,9 @@ const TicketCreation = () => {
             ) : (
                 <button onClick={handleNewIssueClick} id="newIssueButton">New Issue</button>
             )}
+            <div id="ticketLinkContainer">
+                <Link to='/'>Back Home</Link>
+            </div>
         </div>
     )
 }
