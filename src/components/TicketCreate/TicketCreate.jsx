6b63aeea31c61a6f4a146ddb623e38ed77edd8a9
NOAH MORRISON <morrison.noah@gmail.com>
2024-01-26 11:51:18 -0800

    Testing!

--- src/components/TicketCreate/TicketCreate.jsx
+++ src/components/TicketCreate/TicketCreate.jsx
@@ -139,6 +139,7 @@ const TicketCreation = () => {
                                         <tr key={index} className="expandedRowContainer">
                                             <td colSpan="4">
                                                 <div className="expandedRow">
+                                                    {/* TESTING! */}
                                                     <div className="fullDescription">&nbsp;<span className="ticketDescClose" onClick={() => setSelectedTicketIndex(null)}>X</span>  &nbsp;<span className="ticketDownArrow">↳</span>&nbsp; {ticket.description}</div>
                                                 </div>
                                             </td>
