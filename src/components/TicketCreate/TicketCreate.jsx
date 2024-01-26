52030a39e6562d50d84fc476daeb168d36dbafc2
Noah Morrison <103166603+Leaf-mane@users.noreply.github.com>
2024-01-26 12:08:05 -0800

    Update TicketCreate.jsx
    unfark merge

--- src/components/TicketCreate/TicketCreate.jsx
+++ src/components/TicketCreate/TicketCreate.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect  } from "react";
-import { Link } from "react-router-dom";
+import { Link, createRoutesFromElements } from "react-router-dom";
 import { useSupabase } from '../../SupabaseContext';
 import './ticket.css';
 
@@ -13,26 +13,18 @@ const TicketCreation = () => {
         description: ""
     });
 
-    // currentUser data
-    const [currentUser, setCurrentUser] = useState('Guest')
-    supabase.auth.getUser().then(user => {
-        const employee = user.data.user.user_metadata.display_name
-        setCurrentUser(employee)
-    })
-
     const [showForm, setShowForm] = useState(false);
     // tickets in the Outstanding Ticket Status area
     const [pendingTickets, setPendingTickets] = useState([]);
     // state to select ticket by click
     const [selectedTicketIndex, setSelectedTicketIndex] = useState()
     // function to change state of form to show form or new issue button
-    const handleNewIssue = () => { setShowForm(true); }
+    const handleNewIssueClick = () => { setShowForm(true); }
     const handleCloseForm = () => { setShowForm(false); }
 
     // fetch all tickets from data base on inital load
     useEffect(() => {
         const fetchTickets = async () => {
-            
             const { data, error } = await supabase
                 .from('taskissue')
                 .select('*');
@@ -51,8 +43,9 @@ const TicketCreation = () => {
     // add a issue to database and append to table 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log(supabase.auth.getUser())
     
-        setPendingTickets([submittedTicket, ...pendingTickets]);
+        setPendingTickets([...pendingTickets, submittedTicket]);
     
         const { data, error } = await supabase
             .from('taskissue')
@@ -117,11 +110,60 @@ const TicketCreation = () => {
     
 
     return (
-        <div className="ticketPageContainer">    
-            <div id=" ">
+        <div className="ticketPageContainer">
+            <div id="currentUserContainer">
                 <p>Welcome, {currentUser}</p>
             </div>
-            {/* Show Form or Outstanding Tickets Status */}
+            <div className="ticketStatusContainer">
+                <h2>Outstanding Ticket Status</h2>
+                {/* Tickets table */}
+                {pendingTickets.length > 0 && (
+                    <table className="ticketsTable">
+                        <thead>
+                            <tr className="ticketHeaderContainer">
+                                <th className="headerCell">Category</th>
+                                <th className="locationCell">Location</th>
+                                <th className="descriptionCell">Description</th>
+                                <th className="actionCell">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className="ticketTableBody">
+                            {pendingTickets.map((ticket, index) => (
+                                <React.Fragment key={index}>
+                                    <tr key={index} onClick={() => setSelectedTicketIndex(index)}>
+                                        <td className="categoryCell">{ticket.category}</td>
+                                        <td className="locationCell">{ticket.location}</td>
+                                        <td className="descriptionCell, descriptionText">{ticket.description}</td>
+                                        <td className="actionCell">
+                                            <button onClick={() => handleDeleteTicket(index)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                    {selectedTicketIndex === index && (
+                                        <tr key={index} className="expandedRowContainer">
+                                            <td colSpan="4">
+                                                <div className="expandedRow">
+                                                    {/* TESTING! */}
+                                                    <div className="fullDescription">&nbsp;<span className="ticketDescClose" onClick={() => setSelectedTicketIndex(null)}>X</span>  &nbsp;<span className="ticketDownArrow">↳</span>&nbsp; {ticket.description}</div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )}
+
+                                </React.Fragment>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+                {/* Expanded row container outside the table */}
+                {/* {selectedTicketIndex !== undefined && (
+                    <div className="expandedRowContainer">
+                        <div className="expandedRow">
+                            <div className="fullDescription">{pendingTickets[selectedTicketIndex].description}</div>
+                        </div>
+                    </div>
+                )} */}
+            </div>
+            {/* Show Form or new Issue button */}
             {showForm ? (
                 <div className="ticketFormContainer">
                     <div id='ticketFormClose' onClick={handleCloseForm}>x</div>
@@ -174,49 +216,7 @@ const TicketCreation = () => {
                     </form>
                 </div>
             ) : (
-                <>
-                    <div className="ticketStatusContainer">
-                    <h2>Outstanding Ticket Status</h2>
-                    {/* Tickets table */}
-                    {pendingTickets.length > 0 && (
-                        <table className="ticketsTable">
-                            <thead>
-                                <tr className="ticketHeaderContainer">
-                                    <th className="headerCell">Category</th>
-                                    <th className="locationCell">Location</th>
-                                    <th className="descriptionCell">Description</th>
-                                    <th className="actionCell">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody className="ticketTableBody">
-                                {pendingTickets.map((ticket, index) => (
-                                    <React.Fragment key={index}>
-                                        <tr key={index} onClick={() => setSelectedTicketIndex(index)}>
-                                            <td className="categoryCell">{ticket.category}</td>
-                                            <td className="locationCell">{ticket.location}</td>
-                                            <td className="descriptionCell, descriptionText">{ticket.description}</td>
-                                            <td className="actionCell">
-                                                <button onClick={() => handleDeleteTicket(index)}>Delete</button>
-                                            </td>
-                                        </tr>
-                                        {selectedTicketIndex === index && (
-                                            <tr key={index} className="expandedRowContainer">
-                                                <td colSpan="4">
-                                                    <div className="expandedRow">
-                                                        <div className="fullDescription">&nbsp;<span className="ticketDownArrow">↳</span>&nbsp; {ticket.description}</div>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        )}
-
-                                    </React.Fragment>
-                                ))}
-                            </tbody>
-                        </table>
-                    )}
-                </div>
-                <button onClick={handleNewIssue} id="newIssueButton">New Issue</button>
-                </>
+                <button onClick={handleNewIssueClick} id="newIssueButton">New Issue</button>
             )}
             <div id="ticketLinkContainer">
                 <Link to='/'>Back Home</Link>
