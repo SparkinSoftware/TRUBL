f509567f139415df10d5519dec3ec1ec8bf17353
NOAH MORRISON <morrison.noah@gmail.com>
2024-02-01 14:59:20 -0800

    add official nav buttons remove unofficial nav links

--- src/components/Nightmode/NightModeToggle.jsx
+++ src/components/Nightmode/NightModeToggle.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
 import './NightModeToggle.css';
 import {useNightMode} from './NightModeContext.jsx';
+import { Link, useLocation } from 'react-router-dom'
 
 const NightModeToggle = () => {
   const { toggleNightMode } = useNightMode();
-
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login'
+  const isHomePage = location.pathname === '/landing'
   return (
     <div className="siteTopbar">
       <label className="toggle-label">
         <input type="checkbox" onChange={toggleNightMode} className="toggle-input" />
         <span className="toggle-slider"></span>
       </label>
+      {!isLoginPage && (
+      <>
+        <label className="backButtonContainer">
+          <div className="backButtonBG">
+            <Link to="/login">
+              <button className="backButton">Logout</button>
+            </Link>
+          </div>
+        </label>
+        {!isHomePage && (
+          <label className="homeButtonContainer">
+            <div className="homeButtonBG">
+              <Link to="/landing">
+                <button className="homeButton">Home</button>
+              </Link>
+            </div>
+          </label>
+      )}
+      </>
+      )}
     </div>
   );
 };
